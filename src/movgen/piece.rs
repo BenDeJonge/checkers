d8698407f4cdc1f9459fa631774c98c2613fa84a
Ben De Jonge <ben.de.jonge1996@gmail.com>
2026-07-22 23:31:10 +0200

    feat: add methods for different char representations

--- src/movgen/piece.rs
+++ src/movgen/piece.rs
@@ -28,6 +28,8 @@ impl Piece {
             Self::King(_) => 1_000,
         }
     }
+
+    /// Get a single-space character to populate an ASCII chess board.
     pub fn board_representation(&self) -> char {
         match self {
             Self::King(Color::White) => 'K',
@@ -44,23 +46,47 @@ impl Piece {
             Self::Pawn(Color::Black) => 'p',
         }
     }
+
+    /// Annotate moves in algebraic notation e.g., `1. e4 e5 2. Ke2! Ke7!!`.
+    pub fn algebraic_name(&self) -> char {
+        match self {
+            Self::King(_) => 'K',
+            Self::Queen(_) => 'Q',
+            Self::Rook(_) => 'R',
+            Self::Bishop(_) => 'B',
+            Self::Knight(_) => 'N',
+            Self::Pawn(_) => '\0', // Pawn moves are written as simply `e4`.
+        }
+    }
+
+    /// Annotate moves in figurine algebraic notation e.g., `1. e4 e5 2. ♔e2! ♔e7!!`.
+    pub fn figurine(&self) -> char {
+        match self {
+            Self::King(_) => '♔',
+            Self::Queen(_) => '♕',
+            Self::Rook(_) => '♖',
+            Self::Bishop(_) => '♗',
+            Self::Knight(_) => '♘',
+            Self::Pawn(_) => '\0', // Pawn moves are written as simply `e4`.
+        }
+    }
 }
 
 impl Display for Piece {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let ch = match *self {
-            Self::Pawn(Color::White) => '♙',
-            Self::Knight(Color::White) => '♘',
-            Self::Bishop(Color::White) => '♗',
-            Self::Rook(Color::White) => '♖',
-            Self::Queen(Color::White) => '♕',
             Self::King(Color::White) => '♔',
-            Self::Pawn(Color::Black) => '♟',
-            Self::Knight(Color::Black) => '♞',
-            Self::Bishop(Color::Black) => '♝',
-            Self::Rook(Color::Black) => '♜',
-            Self::Queen(Color::Black) => '♛',
+            Self::Queen(Color::White) => '♕',
+            Self::Rook(Color::White) => '♖',
+            Self::Bishop(Color::White) => '♗',
+            Self::Knight(Color::White) => '♘',
+            Self::Pawn(Color::White) => '♙',
             Self::King(Color::Black) => '♚',
+            Self::Queen(Color::Black) => '♛',
+            Self::Rook(Color::Black) => '♜',
+            Self::Bishop(Color::Black) => '♝',
+            Self::Knight(Color::Black) => '♞',
+            Self::Pawn(Color::Black) => '♟',
         };
         write!(f, "{ch}")
     }
